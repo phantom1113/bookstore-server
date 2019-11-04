471f5ecec96155dd80177d0ed11c8f612441cfdf
Phantom <lean66812@gmail.com>
2019-11-04 13:58:33 +0700

    add pagination function

--- routes/api/books.js
+++ routes/api/books.js
@@ -32,7 +32,7 @@ router.get('/pagination', (req,res) => {
     //let filter = {};
     let page = parseInt(req.query.page) || 1;
     //filter.category = req.query.category
-    let perPage = 8;
+    let perPage = 1;
     let start = (page - 1) * perPage;
     let end = page * perPage;
     console.log(req.query.category,page);
